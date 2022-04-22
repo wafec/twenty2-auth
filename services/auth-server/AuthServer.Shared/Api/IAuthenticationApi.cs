795329f3c93380c7e79c1b1d8fdca1b4ef2418e5
Wallace <wallacefcardoso@gmail.com>
2022-04-21 21:40:31 -0300

    auth-server: create api and shared library

---
+++ services/auth-server/AuthServer.Shared/Api/IAuthenticationApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Shared.Api
+{
+    public interface IAuthenticationApi
+    {
+        object Authenticate(string username, string password);
+    }
+}

2717eb59a687f32ab5b94ac327e05950f56ccb26
Wallace <wallacefcardoso@gmail.com>
2022-05-25 15:51:02 -0300

    claims: cleaning uo

--- services/auth-server/auth_api/src/test/java/twenty2/auth/api/dao/UserDaoTest.java
+++ services/auth-server/auth_api/src/test/java/twenty2/auth/api/dao/UserDaoTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
-
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.equalTo;
